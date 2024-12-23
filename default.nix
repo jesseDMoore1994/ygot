dc39f723993697260e3867a3f6a378b84048a524
Jesse Moore <jesse@jessemoore.dev>
2024-12-22 19:57:54 -0600

    feat(nix): add simple flake setup

---
+++ default.nix
@@ -0,0 +1,27 @@
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+, buildGoApplication ? pkgs.buildGoApplication
+}:
+
+buildGoApplication {
+  pname = "myapp";
+  version = "0.1";
+  pwd = ./.;
+  src = ./.;
+  modules = ./gomod2nix.toml;
+  excludedPackages = [
+    "./demo/getting_started"
+    "./demo/uncompressed"
+    "./integration_tests/uncompressed/uschema"
+  ];
+  doCheck = false;
+}
